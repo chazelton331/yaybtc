2ca2ac27f8448b4ccc7c137987b5c72522a7e2fc
Cliff H <cliffhazelton@gmail.com>
2017-04-18 08:35:13 -0400

    Added a little bit of javascript to manage enable/disable of the buy/sell functionality.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+BtcApp = {};
+
+BtcApp.enableToggle = function _enableToggle() {
+  $("#toggle-link").click(function(ev) {
+    ev.preventDefault();
+
+    $.ajax({
+      url: "/accounts/toggle.json",
+      type: "POST",
+      success: function(data, textStatus, jQxhr) {
+        if (data.enabled) {
+          $(".buy-sell").removeClass("auto-buy-sell-disabled");
+          $(".buy-sell").addClass(   "auto-buy-sell-enabled" );
+          $(".buy-sell").text("Auto Buy/Sell is enabled for this account. This app will execute buy/sell orders based on current trends.");
+          $("#toggle-link").text("Disable auto buy/sell");
+        } else if (!data.enabled) {
+          $(".buy-sell").removeClass("auto-buy-sell-enabled" );
+          $(".buy-sell").addClass(   "auto-buy-sell-disabled");
+          $(".buy-sell").text("Auto Buy/Sell is disabled for this account. You can add BTC to the wallet but it will not execute any buy/sell orders.");
+          $("#toggle-link").text("Enable auto buy/sell");
+        }
+      }
+    });
+  });
+};
--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,11 +5,12 @@ class AccountsController < ApplicationController
       @user.auto_buy_sell_enabled = !@user.auto_buy_sell_enabled
       @user.save
 
-      redirect_to root_path, status: 201
-    else
-      redirect_to root_path, status: 401
-    end
+      @status = 201
 
+      render json: { enabled: @user.auto_buy_sell_enabled }, status: 201
+    else
+      render json: { errors: "Not found" }, status: 401
+    end
   end
 
 end
