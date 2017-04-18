01a43a1104df3a9bb1566cd84d766e48a6f5a32b
Cliff H <cliffhazelton@gmail.com>
2017-04-18 08:54:51 -0400

    Added code to invoke the BuySellAnalyzer when new Status records are saved

--- app/models/sentiment_status.rb
+++ app/models/sentiment_status.rb
@@ -12,10 +12,18 @@ class SentimentStatus < ApplicationRecord
 
   validates :score, presence: true
 
+  after_create :invoke_buy_sell_analyzer
+
   def self.save_current_values
     reddit_sentiment_score = RedditQuery.new.fetch_sentiment_score
     total_score            = [ reddit_sentiment_score ].inject(:+)
 
     create(score: total_score)
+  end
+
+  private
+
+  def invoke_buy_sell_analyzer
+    BuySellAnalyzer.new.process
   end
 end
--- spec/models/bitcoin_status_spec.rb
+++ spec/models/bitcoin_status_spec.rb
@@ -17,6 +17,17 @@ RSpec.describe BitcoinStatus, type: :model do
     it { should validate_presence_of(:price) }
   end
 
+  context "callbacks" do
+    describe "#after_create" do
+      it "invokes the BuySellAnalyzer" do
+        buy_sell_analyzer_double = double()
+        expect(buy_sell_analyzer_double).to receive(:process)
+        expect(BuySellAnalyzer).to receive(:new).and_return(buy_sell_analyzer_double)
+        BitcoinStatus.create(price: 1234.56)
+      end
+    end
+  end
+
   it "saves a new record" do
     VCR.use_cassette :winkdex_price do
       expect { BitcoinStatus.save_current_values }.to change { BitcoinStatus.count }.by(1)
