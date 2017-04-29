f0d6b9c3d847daffa14d658da717a78aad52fa2b
Cliff H <cliffhazelton@gmail.com>
2017-04-29 13:52:50 -0400

    Consolidated debug interface into the home dashboard

--- app/controllers/debug_controller.rb
+++
@@ -1,9 +0,0 @@
-class DebugController < ApplicationController
-
-  def index
-    @sentiment_statuses = SentimentStatus.order('created_at DESC').limit(25)
-    @bitcoin_statuses   =   BitcoinStatus.order('created_at DESC').limit(25)
-    @users              =            User.order('created_at DESC').limit(10)
-  end
-
-end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @sentiment_statuses = SentimentStatus.order('created_at DESC').limit(25)
+    @bitcoin_statuses   =   BitcoinStatus.order('created_at DESC').limit(25)
   end
 
 end
